b1d918d020aef1949a2cb62887c8283388a7991f
Daryl St Paul <daryl.stpaul@gmail.com>
2026-02-21 21:54:47 +0100

    event form page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { Home } from './home/home';
 import { QrCode } from './qr-code/qr-code';
+import { EventForm } from './event-form/event-form';
 
 export const routes: Routes = [
      { path: '', component: Home },
      {path: 'QrCode', component: QrCode},
+     {path: 'EventForm', component: EventForm},
      { path: '**', redirectTo: '' }
 ];
---
+++ src/app/event-form/event-form.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-event-form',
+  imports: [RouterLink],
+  templateUrl: './event-form.html',
+  styleUrl: './event-form.scss',
+})
+export class EventForm {
+
+}
