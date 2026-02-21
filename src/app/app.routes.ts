f1531dc7d74def368b661aca191df6d226342270
Daryl St Paul <daryl.stpaul@gmail.com>
2026-02-21 21:35:49 +0100

    home and qr code pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { QrCode } from './qr-code/qr-code';
+
+export const routes: Routes = [
+     { path: '', component: Home },
+     {path: 'QrCode', component: QrCode},
+     { path: '**', redirectTo: '' }
+];
---
+++ src/app/qr-code/qr-code.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-qr-code',
+  imports: [RouterLink],
+  templateUrl: './qr-code.html',
+  styleUrl: './qr-code.scss',
+})
+export class QrCode {
+
+}
