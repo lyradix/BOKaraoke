f29832734211ab435c5fb817beded5afa00a8132
Daryl St Paul <daryl.stpaul@gmail.com>
2026-02-23 16:22:17 +0100

    function to post room data

--- src/app/event-form/event-form.ts
+++ src/app/event-form/event-form.ts
@@ -1,27 +1,56 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { QRCodeComponent } from 'angularx-qrcode';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+// import { QRCodeComponent } from 'angularx-qrcode';
+// import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 
 @Component({
   selector: 'app-event-form',
-  imports: [RouterLink, QRCodeComponent],
+  imports: [RouterLink],
   templateUrl: './event-form.html',
   styleUrl: './event-form.scss',
 })
 export class EventForm {
+  nameValue: string = '';
+  placeValue: string = '';
+  dateValue: string = '';
 
-  public myAngularxQrCode: string = "";
-  public qrCodeDownloadLink: SafeUrl = "";
+  constructor(private http: HttpClient) {}
 
-  constructor (private sanitizer: DomSanitizer) {
-    this.myAngularxQrCode = 'http://192.168.1.164:54348/Contact';
+  name(event: Event) {
+    this.nameValue = (event.target as HTMLInputElement).value;
   }
 
-  onChangeURL(url: SafeUrl) {
-    this.qrCodeDownloadLink = url;
+  place(event: Event) {
+    this.placeValue = (event.target as HTMLInputElement).value;
+  }
+  
+  date(event: Event) {
+    this.dateValue = (event.target as HTMLInputElement).value;
   }
 
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    if(this.nameValue && this.placeValue && this.dateValue) {
+
+      const eventData = {
+        name: this.nameValue,
+        place: this.placeValue,
+        date: this.dateValue
+      };
+
+      this.http.post('http://192.168.1.164:8000/postRoom', eventData).subscribe(
+        // (response) => console.log('ok', response),
+         (response) =>console.log(eventData),
+       
+        (error) => console.error('Erreur', error)
+      );
+    } else {
+      console.warn('Veuillez remplir tous les champs du formulaire.');
+
+    }
+  }
   
 }
--- src/app/qr-code/qr-code.ts
+++ src/app/qr-code/qr-code.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { QRCodeComponent } from 'angularx-qrcode';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 
 @Component({
   selector: 'app-qr-code',
-  imports: [RouterLink],
+  imports: [RouterLink, QRCodeComponent],
   templateUrl: './qr-code.html',
   styleUrl: './qr-code.scss',
 })
 export class QrCode {
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    this.myAngularxQrCode = 'http://192.168.1.164:54348/Contact';}
+  public myAngularxQrCode: string = "";
+  public qrCodeDownloadLink: SafeUrl = "";
+   onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
+  }
+
+ 
 }
